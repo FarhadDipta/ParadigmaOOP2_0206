18b72c0f780b9ac54e0270dfa5db2ebd3c00d1b3
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-05-28 15:28:04 +0700

    menambahkan classbaseclass di tugas noPewarisan

---
+++ noPewarisan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan() {
+        cout << "Hello saya dari function dari base class";
+    }
+};
+
+int main() {
+
+}
